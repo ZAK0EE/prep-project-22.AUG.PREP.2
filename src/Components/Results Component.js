dbc3026143141dd0435d5c4659a8d13da75e9b27
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-08-10 18:19:48 +0200

    Added forecast data for the result component

--- src/Components/Results Component.js
+++ src/Components/Results Component.js
@@ -1,10 +1,11 @@
 
-export default function ResultsComponent({isLoaded, results}) {
+export default function ResultsComponent({isLoaded, results, foreResult}) {
 
     return (
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
           {console.log(results)}
+          {console.log(foreResult)}
           {isLoaded && results && <>
             <h3>{results.weather[0].main}</h3>
             <p>Feels like {results.main.feels_like}°C</p>
